d835b04f99c115cf87a7342b30422db318b34466
AlexM <alex.muromtsev@gmail.com>
2023-01-25 03:03:04 +0300

    add project

---
+++ Extension/UIViewExtension.swift
@@ -0,0 +1,15 @@
+//
+//  UIViewExtension.swift
+//  WeatherApp
+//
+//  Created by Alex M on 30.12.2022.
+//
+
+import UIKit
+
+extension UIView {
+    static var identifier: String {
+        return String(describing: self)
+    }
+}
+
